f91825f8b7af9d60670a0b399f4bf67cbd0386c2
fastf0rward <peterpauloldenzeel@gmail.com>
2017-05-10 15:40:03 +0200

    + tabs nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,32 @@ import {FormsModule} from "@angular/forms";
 import {HttpModule} from "@angular/http";
 import {MaterialModule} from "@angular/material";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import 'hammerjs';
-
+import "hammerjs";
 import {AppComponent} from "./app.component";
+import {GroceryListComponent} from "./grocery-list/grocery-list.component";
+import {RouterModule, Routes} from "@angular/router";
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {FlexLayoutModule} from "@angular/flex-layout";
+
+const appRoutes: Routes = [
+  {path: 'grocery-list', component: GroceryListComponent},
+  {path: '', redirectTo: '/grocery-list', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GroceryListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     BrowserAnimationsModule,
-    MaterialModule.forRoot() // TODO shift back to importing single Material Components instead of everything
+    MaterialModule.forRoot(), // TODO shift back to importing single Material Components instead of everything
+    RouterModule.forRoot(appRoutes),
     FlexLayoutModule
   ],
   providers: [],
