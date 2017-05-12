d59b2bf6549519460e97d952c9c3784450724574
fastf0rward <peterpauloldenzeel@gmail.com>
2017-05-12 13:36:26 +0200

    ~ refactor receipt scanner

--- src/app/receipt/receipt.component.ts
+++ src/app/receipt/receipt.component.ts
@@ -12,11 +12,18 @@ export class ReceiptComponent implements OnInit, AfterViewInit {
   @ViewChild('canvas') canvas: any;
   public showVideo: any = false;
 
-  context: any;
+  private context: any;
   comment: string;
   identifier: string;
   longitude: number;
   latitude: number;
+  canvasWidth: number;
+  canvasHeight: number;
+  canvasOffsetLeft: number;
+  canvasOffsetTop: number;
+  navHeight: number;
+  private constraints: any;
+  private preferredDevice: string;
 
   @Input() width: number;
   @Input() height: number;
@@ -25,8 +32,7 @@ export class ReceiptComponent implements OnInit, AfterViewInit {
   }
 
   capture() {
-    // this.context = this.canvas.nativeElement.getContext('2d');
-    this.context.drawImage(this.videoPlayer.nativeElement, 0, 0, this.width, this.height);
+    this.context.drawImage(this.videoPlayer.nativeElement, this.canvasOffsetLeft, this.canvasOffsetTop, this.canvasWidth, this.canvasHeight, 0, 0, this.canvasWidth, this.canvasHeight);
     this.showVideo = true;
   }
 
@@ -54,23 +60,60 @@ export class ReceiptComponent implements OnInit, AfterViewInit {
       'longitude': this.longitude,
       'latitude': this.latitude
     });
-
   }
 
+  getPreferredDevice(): string {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+      console.log("enumerateDevices() not supported.");
+      return;
+    }
+
+    // List cameras and microphones.
+    navigator.mediaDevices.enumerateDevices()
+      .then(function (devices) {
+        devices.forEach(function (device) {
+          console.log(device.kind + ": " + device.label +
+            " id = " + device.deviceId);
+        });
+        return devices[0];
+      })
+      .catch(function (err) {
+        console.log(err.name + ": " + err.message);
+        return false;
+      });
+  }
 
   ngOnInit() {
-    this.width = 320;
-    this.height = 240;
+
+    // select back camera
+    this.preferredDevice = this.getPreferredDevice();
+
+    // set camera constraints
+    this.constraints = {
+      video: {
+        optional: [{sourceId: this.preferredDevice}]
+      }
+    };
+
+    this.width = 500 / 240 * 320;
+    this.height = 500;
+    this.canvasWidth = 240;
+    this.canvasHeight = 464;
+
+    this.navHeight = 57;
+
+    this.canvasOffsetLeft = 0.5 * this.width - 0.5 * this.canvasWidth;
+    this.canvasOffsetTop = (this.height - this.canvasHeight) * 0.5;
   }
 
   ngAfterViewInit() {
     // Get current geolocation
-    this.getGeolocation();
+    //this.getGeolocation();
 
     this.context = this.canvas.nativeElement.getContext('2d');
 
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-      navigator.mediaDevices.getUserMedia({video: true})
+      navigator.mediaDevices.getUserMedia(this.constraints)
         .then(stream => {
           this.videoPlayer.nativeElement.src = window.URL.createObjectURL(stream);
           this.videoPlayer.nativeElement.play();
