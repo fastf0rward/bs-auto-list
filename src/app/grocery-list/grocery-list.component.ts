5a8a3634bf3c03bf6110805f58655abf0442ede1
fastf0rward <peterpauloldenzeel@gmail.com>
2017-06-06 22:46:08 +0200

    ~ update grocery list methods to connect to Firebase list

--- src/app/grocery-list/grocery-list.component.ts
+++ src/app/grocery-list/grocery-list.component.ts
@@ -1,8 +1,6 @@
 import {Component, OnInit} from "@angular/core";
 import {GroceryListService} from "./grocery-list.service";
 import * as _ from "lodash";
-import {MdDialog} from "@angular/material";
-import {GroceryListExplanationDialogComponent} from "./grocery-list-explanation-dialog.component";
 
 @Component({
   selector: 'app-grocery-list',
@@ -11,27 +9,20 @@ import {GroceryListExplanationDialogComponent} from "./grocery-list-explanation-
 })
 export class GroceryListComponent implements OnInit {
   products: any[];
-  suggestions: any[];
+  suggestions: any[] = [];
   showLoader: boolean;
 
-  constructor(public groceryListService: GroceryListService, private dialog: MdDialog) {
+  constructor(public groceryListService: GroceryListService) {
     this.groceryListService.products.subscribe(_products => {
       this.products = _products;
-      this.suggestions = _.filter(_products, {suggested: true, accepted: false});
+      this.suggestions = _.filter(_products, {accepted: false});
     });
     this.groceryListService.showLoader.subscribe(_show => {
       this.showLoader = _show;
     });
-
-    // reset loader
-    this.groceryListService._showLoaderSubject.next(true);
   }
 
   ngOnInit() {
-    let dialogRef = this.dialog.open(GroceryListExplanationDialogComponent);
-    dialogRef.afterClosed().subscribe(() => {
-      this.groceryListService._showLoaderSubject.next(false);
-    });
   }
 
 
--- src/app/grocery-list/grocery-list.service.ts
+++ src/app/grocery-list/grocery-list.service.ts
@@ -1,72 +1,57 @@
 import {Injectable} from "@angular/core";
 import {BehaviorSubject} from "rxjs/BehaviorSubject";
-import * as _ from "lodash";
+import {FirebaseListObservable} from "angularfire2/database/firebase_list_observable";
+import {AngularFireAuth} from "angularfire2/auth/auth";
+import {AngularFireDatabase} from "angularfire2/database/database";
 
 @Injectable()
 export class GroceryListService {
 
-  private _productsSubject = new BehaviorSubject([]);
-  products = this._productsSubject.asObservable();
-
-  _showLoaderSubject = new BehaviorSubject(true);
+  _showLoaderSubject = new BehaviorSubject(false);
   showLoader = this._showLoaderSubject.asObservable();
 
-  constructor() {
-    let mockSuggestions = [
-      {name: 'Knorr Mix', quantity: '100g', bought: 'vandaag', suggested: true, accepted: false},
-      {name: 'Aardappelen', quantity: '1kg', bought: '3 dagen geleden', suggested: true, accepted: false},
-      {name: 'Gehakt', quantity: '400g', bought: '1 week geleden', suggested: true, accepted: false},
-      {name: 'Pindakaas', quantity: '400g', bought: '3 weken geleden', suggested: true, accepted: false},
-    ];
-    this._productsSubject.next(mockSuggestions);
+  products: FirebaseListObservable<any[]>;
+
+  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+    this.afAuth.auth.signInAnonymously();
+    this.products = af.list('/users/3bjh23b5jh2v35jhg23j5/suggestions'); // TODO dynamic user ID
   }
 
   addProduct(productName: string) {
     if (productName.length === 0) {
       return;
     }
-    let _products = this._productsSubject.getValue();
     let _prod = {
       name: productName,
-      suggested: false,
-      accepted: true
-    };
-    _products.push(_prod);
-    this._productsSubject.next(_products);
+      status: 'created',
+      created: new Date()
+  };
+    this.products.push(_prod);
   }
 
   removeProduct(product: any) {
-    let _products = this._productsSubject.getValue();
-    _products = _.without(_products, product);
-    this._productsSubject.next(_products);
+    this.products.remove(product.$key);
   }
 
   acceptSuggestion(suggestion: any) {
-    let _products = this._productsSubject.getValue();
-    _products.forEach(_prod => {
-      if (_prod.name === suggestion.name) {
-        _prod.accepted = true;
-      }
-    });
-
-    this._productsSubject.next(_products);
+    let acceptedSuggestion = suggestion;
+    acceptedSuggestion.status = 'accepted';
+    this.products.update(acceptedSuggestion.$key, acceptedSuggestion);
   }
 
   acceptAllSuggestions(): void {
-    let _products = this._productsSubject.getValue();
-    _products.forEach(_prod => {
-      if (_prod.suggested) {
-        _prod.accepted = true;
-      }
-    });
-    this._productsSubject.next(_products);
+    this.products
+      .subscribe(_prods => {
+        _prods.forEach(_prod => {
+          _prod.status = 'accepted'
+        });
+      });
   }
 
   rejectSuggestion(suggestion: any) {
-    let _products = this._productsSubject.getValue();
-    _products = _.without(_products, suggestion);
-
-    this._productsSubject.next(_products);
+    let rejectedProduct = suggestion;
+    rejectedProduct.status = 'rejected';
+    this.products.update(rejectedProduct.$key, rejectedProduct);
   }
 
 }
