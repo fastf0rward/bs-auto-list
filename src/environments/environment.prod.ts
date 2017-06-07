ec059151ab4d3967be07b507e140bb756768ce58
fastf0rward <peterpauloldenzeel@gmail.com>
2017-06-07 15:19:49 +0200

    add firebase config to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBR2wCnxgq9WwuvkW2IR9ggslaKkbV8-OM",
+    authDomain: "jill-auto.firebaseapp.com",
+    databaseURL: "https://jill-auto.firebaseio.com",
+    projectId: "jill-auto",
+    storageBucket: "jill-auto.appspot.com",
+    messagingSenderId: "108777278198"
+  }
 };
