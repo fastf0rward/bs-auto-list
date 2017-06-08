065c5ef5789a1b28c6bc067b48c6a271a9cd8d62
fastf0rward <peterpauloldenzeel@gmail.com>
2017-06-08 13:12:43 +0200

    ~ hide top nav in Admin view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from "@angular/core";
 import {GroceryListService} from "./grocery-list/grocery-list.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
@@ -8,10 +9,15 @@ import {GroceryListService} from "./grocery-list/grocery-list.service";
 })
 export class AppComponent {
   userId: string;
+  isUserView: boolean;
 
-  constructor(private groceryListService: GroceryListService) {
+  constructor(private groceryListService: GroceryListService, private router: Router) {
     this.groceryListService.userId.subscribe(_id => {
       this.userId = _id;
+    });
+
+    router.events.subscribe((urlInfo: any) => {
+      this.isUserView = urlInfo.url.includes('admin') == false;
     });
   }
 }
